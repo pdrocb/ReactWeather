65d57a1d1e92690dad52243abd16f5a5499251e4
Pedro Conchas <pdrocb@MacBook-Pro-de-Pedro.local>
2017-02-13 12:35:45 -0600

    Text Changed, disable css

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -38,7 +38,7 @@ var Weather = React.createClass({
 
     return (
       <div>
-        <h3>Get crazy and Fetch weather everywhere</h3>
+        <h3>Get weather everywhere</h3>
         <WeatherForm onSearch={this.handleSearch} />
         {renderMessage()}
       </div>
