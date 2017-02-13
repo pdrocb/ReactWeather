b45db5750b3e76ce579753ce0476c12094d5a0e7
Pedro Conchas <pdrocb@MacBook-Pro-de-Pedro.local>
2017-02-13 00:03:55 -0600

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ var express = require('express');
 // Create our app
 
 var app = express();
+// GWorking with nodejs we can acces to the port using process.env.port
+cost PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-    console.log('Express server is up on port 3000');
+app.listen(PORT, function () {
+    console.log('Express server is up on port ' + PORT);
 });
