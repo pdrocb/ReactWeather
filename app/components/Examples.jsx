4266e958b3e9c376dfc3d8189be19c2c539c1b81
Pedro Conchas <pdrocb@MacBook-Pro-de-Pedro.local>
2017-02-13 18:15:07 -0600

    style examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
+
+// at columns small-12 is by default because it's not specified 
 
 var Examples = () => {
     return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to example page</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few examples to try out:</p>
+        <ol>
+          <li>
+            <Link to='/?location=Guadalajara'>Guadalajara, Jalisco</Link>
+          </li>
+          <li>
+            <Link to='/?location=Motherinlaw'>Mother in law Heart</Link>
+          </li>
+        </ol>
       </div>
     );
 };
