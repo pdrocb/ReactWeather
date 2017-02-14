30df26ea807d3f8853276a652908e61dc74eaed6
Pedro Conchas <pdrocb@MacBook-Pro-de-Pedro.local>
2017-02-13 18:33:20 -0600

    styling about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,19 +3,25 @@ var React = require('react');
 // This can be refactored becase is a stateless component
 // Just show information
 
-// var About = React.createClass({
-//   render: function() {
-//     return (
-//       <h3>About Componen!</h3>
-//     );
-//   }
-// });
-
 var About = () => {
   return (
     <div>
-      <h3>About Component!</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application build on React. I have build this
+        because I don't have anything better to do right now.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react/">React</a> - This was the JS Framework I used.
+        </li>
+        <li>
+          <a href="https://openweathermap.org/">Open Weather Map</a> - I used Open Weather Map to search initially for weather data by city name. But I realized it was better to make stupid weather searches.
+        </li>
+      </ul>
     </div>
   );
 };
